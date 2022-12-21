a74ee51fffad3a3db05baf31633ce5c2457ecf80
Patrik Görling <p.gorling@gmail.com>
2022-12-21 09:53:08 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::fs;
+
+fn part1() {
+    let content = fs::read_to_string("input.txt").expect("Expected input.txt");
+    let lines = content.lines();
+
+    println!("Part1: ");
+}
+
+fn part2() {
+    let content = fs::read_to_string("input.txt").expect("Expected input.txt");
+    let lines = content.lines();
+
+    println!("Part2: ");
+}
+
+fn main() {
+    part1();
+    part2();
+}
